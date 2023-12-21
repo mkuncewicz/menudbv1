d2bd27b7f955394be61de2b80947286fa0e8fe1d
mkunceiwcz <mateusz.kuncewicz@onet.pl>
2023-12-21 15:11:44 +0100

    frontMain

--- src/main/java/com/example/dania/controlerGUI/HomeAddController.java
+++ src/main/java/com/example/dania/controlerGUI/HomeAddController.java
@@ -1,9 +1,17 @@
 package com.example.dania.controlerGUI;
 
+import com.example.dania.dto.DishDto;
+import com.example.dania.entity.Dish;
+import com.example.dania.mapper.DishMapper;
 import com.example.dania.service.DishService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class HomeAddController {
@@ -11,8 +19,14 @@ public class HomeAddController {
     @Autowired
     private DishService dishService;
 
-    @GetMapping("/add")
-    public String addDishToOrder(){
+    @Autowired
+    private DishMapper dishMapper;
+
+    @GetMapping("/add/{catId}")
+    public String addDishToOrder(Model model, @PathVariable Long catId){
+        List<DishDto> dishDtoList = dishMapper.mapToListDto(dishService.getDishesFromCategory(catId));
+
+        model.addAttribute("dishes",dishDtoList);
 
         return "adddish";
     }
